8641bb8b3ecf45b133a2b95d30a0db6a14abaa09
madman66 <37365177+madman66@users.noreply.github.com>
2025-03-01 19:06:11 +0530

    Add files via upload

---
+++ Random Number Guesser.py
@@ -0,0 +1,2 @@
+for i in range(10):
+    print("Welcome to the random nbumber guesser, in easy more you have 10 attempts to get the answer right, this is attempt number ", i+1, ' you have ',9-i,' attempts left')
